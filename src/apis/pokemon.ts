96a034d1dee4f2f87497bfcf1233dab7a59ae3c2
leo_chen <leo_chen@mayohr.com>
2025-03-12 16:08:16 +0800

    add zod

--- src/apis/pokemon.ts
+++ src/apis/pokemon.ts
@@ -1,19 +1,28 @@
-export type Pokemon = {
-	name: string;
-	id: number;
-	sprites: {
-		front_default: string;
-	};
-};
-export type PokemonListResponse = {
-	count: number;
-	next: string | null;
-	previous: string | null;
-	results: {
-		name: string;
-		url: string;
-	}[];
-};
+import { z } from 'zod';
+
+// 定義 Pokemon Schema
+export const PokemonSchema = z.object({
+	name: z.string(),
+	id: z.number(),
+	sprites: z.object({
+		front_default: z.string().url(),
+	}),
+});
+
+export const PokemonListResponseSchema = z.object({
+	count: z.number(),
+	next: z.string().nullable(),
+	previous: z.string().nullable(),
+	results: z.array(
+		z.object({
+			name: z.string(),
+			url: z.string().url(),
+		}),
+	),
+});
+
+export type Pokemon = z.infer<typeof PokemonSchema>;
+export type PokemonListResponse = z.infer<typeof PokemonListResponseSchema>;
 
 export const QUERY_KEY_POKEMON_LIST = 'pokemonList';
 export const LIMIT = 20;
@@ -26,9 +35,27 @@ export const fetchPokemonList = async (
 			`https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=${LIMIT}`,
 		).then((res) => res.json());
 
-		return Promise.all(
-			response.results.map((item) => fetch(item.url).then((res) => res.json())),
+		const parsedResponse = PokemonListResponseSchema.safeParse(response);
+		if (!parsedResponse.success) {
+			console.error('Invalid Pokémon List Response', parsedResponse.error);
+			return [];
+		}
+
+		const data = await Promise.all(
+			parsedResponse.data.results.map(async ({ url }) => {
+				const pokemon: Pokemon = await fetch(url).then((res) => res.json());
+
+				const parsedPokemon = PokemonSchema.safeParse(pokemon);
+				if (parsedPokemon.success) {
+					return parsedPokemon.data;
+				} else {
+					console.error('Invalid Pokémon Data', parsedPokemon.error);
+					return null;
+				}
+			}),
 		);
+
+		return data.filter((p) => p !== null);
 	} catch (error) {
 		console.error('Failed to fetch Pokémon List', error);
 		return [];
@@ -36,15 +63,23 @@ export const fetchPokemonList = async (
 };
 
 export const QUERY_KEY_POKEMON_DETAIL = 'pokemonDetail';
-export const fetchPokemonDetail = (
+export const fetchPokemonDetail = async (
 	id: number | string,
 ): Promise<Pokemon | null> => {
 	try {
-		return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
-			res.json(),
+		const response = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(
+			(res) => res.json(),
 		);
+
+		const parsed = PokemonSchema.safeParse(response);
+		if (!parsed.success) {
+			console.error('Invalid Pokémon Detail Response', parsed.error);
+			return null;
+		}
+
+		return parsed.data;
 	} catch (error) {
 		console.error('Failed to fetch Pokémon Detail', error);
-		return Promise.resolve(null);
+		return null;
 	}
 };
